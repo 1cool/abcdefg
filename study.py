c6f1d664b90540e8a74d8c7d5bf063bf47e47d40
1cool <huichun.du@baozun.com>
2018-10-26 18:14:02 +0800

    hahh

--- study.py
+++ study.py
@@ -21,7 +21,7 @@ EXAM_AUTO_SUBMIT = False
 
 def post(url, data, raw=False, sleep=True):
     device = ''.join(random.choice(string.ascii_letters) for x in range(random.randint(6, 15)))
-    s.headers.update({'User-Agent': f'Dalvik/2.1.0 (Linux; U; Android 9.0.0; {device})'})
+    s.headers.update({'User-Agent': 'Dalvik/2.1.0 (Linux; U; Android 9.0.0; {device})'})
     r = s.post(SERVER + url, data=data, verify=SSL_VERIFY)
     if sleep:
         time.sleep(0.5 + random.random())
@@ -40,14 +40,17 @@ def to_json(j):
 
 
 def login():
-    account = input('Account(Phone):')
-    password = getpass(prompt='Password:')
+    # account = input('Account(Phone):')
+    # password = getpass(prompt='Password:')
+    account = '13273901621'
+    password = 'vivox23'
     assert account or password
 
     p = {'account': account, 'password': password, 'areaCode': '86', 'appVersion': '4.0.6', 'clientType': '1',
          'imei': uuid.uuid4().hex}
     pp = {'paramJsonStr': utils.rsa_encrypt_public(public_key, json.dumps(p, separators=(',', ':'))),
           'timeNote': '1515340800'}
+
     d = post('/newuser/userLoginByAccount', pp, sleep=False)
     u = d['userId']
     uu = d['userUUID']
@@ -59,13 +62,32 @@ def login():
     logger.info(ai)
     logger.info(ui)
     n = ui['realName']
-    logger.info(f'{u} {uu} {n}')
+    logger.info('{u} {uu} {n}')
     with open('userinfo.py', 'w+', encoding='utf-8') as f:
-        f.writelines(f'USER = {u}\n')
-        f.writelines(f'UUID = "{uu}"\n')
-        f.writelines(f'NAME = "{n}"\n')
+        f.writelines('USER = {u}\n')
+        f.writelines('UUID = "{uu}"\n')
+        f.writelines('NAME = "{n}"\n')
     logger.info('Login OK.')
     return u, uu, n
+
+
+def save_record(dic, lesson, is_section):
+    if studied is not None and 'L{dic["id"]}' in studied and studied['L{dic["id"]}']['watchState'] == 1:
+        return
+    p = {'deviceId': app_key, 'uuid': uu, 'timeNote': '1515340800'}
+    rt = post('/student/tutorial/getSaveStudyRecordToken', p)
+    token = utils.rsa_decrypt_public(public_key, rt)
+    video_time = dic['videoSec']
+    j = {'lessonId': lesson['id'], 'learnTime': str(timedelta(seconds=video_time)), 'userId': user,
+         'personalCourseId': link_course_id, 'recruitId': recruit_id, 'chapterId': lesson['chapterId'],
+         'sourseType': 3, 'playTimes': video_time, 'videoId': dic['videoId'], 'token': token, 'deviceId': app_key}
+    if is_section:
+        j['lessonVideoId'] = dic['id']
+    json_str = to_json(j)
+    p = {'jsonStr': json_str, 'secretStr': utils.rsa_encrypt(yzm_key, json_str), 'timeNote': '1515340800',
+         'uuid': uu, 'versionKey': '2'}
+    rt = post('/student/tutorial/saveStudyRecordByTokenEncry', p)
+    logger.info(dic['name'] + rt)
 
 
 if __name__ == '__main__':
@@ -73,9 +95,9 @@ if __name__ == '__main__':
     logger = logging.getLogger()
     logger.info('I love studying! Study makes me happy!')
 
-    rsa_key = RSA.import_key(open('key.pem', 'r').read())
-    public_key = RSA.import_key(open('public.pem', 'r').read())
-    yzm_key = RSA.import_key(open('yzm.pem', 'r').read())
+    rsa_key = RSA.import_key(open('/var/www/wisdomtree/key.pem', 'r').read())
+    public_key = RSA.import_key(open('/var/www/wisdomtree/public.pem', 'r').read())
+    yzm_key = RSA.import_key(open('/var/www/wisdomtree/yzm.pem', 'r').read())
     app_key = utils.md5_digest(str(uuid.uuid4()).replace('-', ''))
 
     s = requests.Session()
@@ -87,7 +109,7 @@ if __name__ == '__main__':
         user = userinfo.USER
         uu = userinfo.UUID
         name = userinfo.NAME
-        if input(f'Current user:{user} {name}:[y/n]') != 'y':
+        if input('Current user:{user} {name}:[y/n]') != 'y':
             user, uu, name = login()
     except:
         user, uu, name = login()
@@ -97,48 +119,39 @@ if __name__ == '__main__':
     course_id, recruit_id, link_course_id = 0, 0, 0
     if d is None:
         logger.info('No studying courses.')
-        exit()
     for course in d:
-        if input(f'{course["courseName"]} {course["className"]}:[y/n]') == 'y':
-            course_id = course['courseId']
-            recruit_id = course['recruitId']
-            link_course_id = course['linkCourseId']
-            break
-    if course_id == 0:
-        exit()
+        # if input('{course["courseName"]} {course["className"]}:[y/n]') == 'y':
+        course_id = course['courseId']
+        recruit_id = course['recruitId']
+        link_course_id = course['linkCourseId']
+        p = {'recruitId': recruit_id, 'courseId': course_id, 'userId': user}
+        chapter_list = post('/courseStudy/course/getChaptersInfoOnly', p)['chapterList']
+        studied = post('/appserver/student/queryStudiedLessonsNew', p)['studiedInfos']
+        for chapter in chapter_list:
+            for lesson in chapter['lessonList']:
+                if lesson['sectionList'] is not None:
+                    for section in lesson['sectionList']:
+                        save_record(section, lesson, True)
+                else:
+                    save_record(lesson, lesson, False)
 
+        logger.info('Videos done.')
+        # break
+    # if course_id == 0:
+    #     exit()
 
-    def save_record(dic, lesson, is_section):
-        if studied is not None and f'L{dic["id"]}' in studied and studied[f'L{dic["id"]}']['watchState'] == 1:
-            return
-        p = {'deviceId': app_key, 'uuid': uu, 'timeNote': '1515340800'}
-        rt = post('/student/tutorial/getSaveStudyRecordToken', p)
-        token = utils.rsa_decrypt_public(public_key, rt)
-        video_time = dic['videoSec']
-        j = {'lessonId': lesson['id'], 'learnTime': str(timedelta(seconds=video_time)), 'userId': user,
-             'personalCourseId': link_course_id, 'recruitId': recruit_id, 'chapterId': lesson['chapterId'],
-             'sourseType': 3, 'playTimes': video_time, 'videoId': dic['videoId'], 'token': token, 'deviceId': app_key}
-        if is_section:
-            j['lessonVideoId'] = dic['id']
-        json_str = to_json(j)
-        p = {'jsonStr': json_str, 'secretStr': utils.rsa_encrypt(yzm_key, json_str), 'timeNote': '1515340800',
-             'uuid': uu, 'versionKey': '2'}
-        rt = post('/student/tutorial/saveStudyRecordByTokenEncry', p)
-        logger.info(dic['name'] + rt)
-
-
-    p = {'recruitId': recruit_id, 'courseId': course_id, 'userId': user}
-    chapter_list = post('/courseStudy/course/getChaptersInfoOnly', p)['chapterList']
-    studied = post('/appserver/student/queryStudiedLessonsNew', p)['studiedInfos']
-    for chapter in chapter_list:
-        for lesson in chapter['lessonList']:
-            if lesson['sectionList'] is not None:
-                for section in lesson['sectionList']:
-                    save_record(section, lesson, True)
-            else:
-                save_record(lesson, lesson, False)
-
-    logger.info('Videos done.')
+    # p = {'recruitId': recruit_id, 'courseId': course_id, 'userId': user}
+    # chapter_list = post('/courseStudy/course/getChaptersInfoOnly', p)['chapterList']
+    # studied = post('/appserver/student/queryStudiedLessonsNew', p)['studiedInfos']
+    # for chapter in chapter_list:
+    #     for lesson in chapter['lessonList']:
+    #         if lesson['sectionList'] is not None:
+    #             for section in lesson['sectionList']:
+    #                 save_record(section, lesson, True)
+    #         else:
+    #             save_record(lesson, lesson, False)
+    #
+    # logger.info('Videos done.')
 
     if TAKE_EXAMS is False:
         exit()
@@ -168,7 +181,7 @@ if __name__ == '__main__':
         for exam_question in ids:
             question_ids.append(str(exam_question['questionId']))
 
-        j = {'examId': exam_id, 'questionIds': f'[{",".join(question_ids)}]', 'uuid': uuid}
+        j = {'examId': exam_id, 'questionIds': '[{",".join(question_ids)}]', 'uuid': uuid}
         p = {**j, 'secretStr': utils.rsa_encrypt(yzm_key, to_json(j)), 'versionKey': '2'}
         questions = post('/student/exam/getQuestionDetailInfoFromTeacherEncry', p)
         undone_question_ids = question_ids[:]
@@ -188,12 +201,12 @@ if __name__ == '__main__':
                 question['possibleAnswers'] = combinations
         while len(undone_question_ids) > 0:
             p = {'recruitId': recruit_id, 'examId': exam_id, 'stuExamId': student_exam_id,
-                 'questionIds': f'[{",".join(undone_question_ids)}]', 'userId': user}
+                 'questionIds': '[{",".join(undone_question_ids)}]', 'userId': user}
             scores = post('/student/exam/getQuestionDoneState', p)
             undone_question_ids[:] = itertools.filterfalse(
                 lambda q: scores[q]['score'] == questions[q]['qscore'] or len(questions[q]['possibleAnswers']) == 0,
                 undone_question_ids)
-            logger.info(f'{len(undone_question_ids)} questions left.')
+            logger.info('{len(undone_question_ids)} questions left.')
 
             for question_id in undone_question_ids:
                 question = questions[question_id]
@@ -219,6 +232,6 @@ if __name__ == '__main__':
               'type': exam_type, 'versionKey': '2'}
         raw = post('/newexam/submitExamInfoEncry', pb, raw=True)
         rt = json.loads(raw.replace('"{', '{').replace('}"', '}').replace('\\', ''))['rt']
-        logger.info(f'{rt["messages"]} Score: {rt["errorInfo"]["score"]}')
+        logger.info('{rt["messages"]} Score: {rt["errorInfo"]["score"]}')
 
     logger.info('Exams done.')
